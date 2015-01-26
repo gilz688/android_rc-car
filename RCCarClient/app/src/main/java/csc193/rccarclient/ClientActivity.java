748d799597b434f2f09c025937ed67a90414d99e
Gilmore <eromlig.ac@gmail.com>
2015-01-26 11:50:48 +0800

    displayed refresh action item

--- RCCarClient/app/src/main/java/csc193/rccarclient/ClientActivity.java
+++ RCCarClient/app/src/main/java/csc193/rccarclient/ClientActivity.java
@@ -1,18 +1,14 @@
 package csc193.rccarclient;
 
-import android.graphics.Color;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
-import android.view.Window;
-import android.view.WindowManager;
 
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 
