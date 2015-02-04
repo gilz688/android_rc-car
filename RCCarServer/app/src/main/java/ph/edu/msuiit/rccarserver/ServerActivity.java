d2cc007ac7516d744c79015a6f17e58d29ef4355
jal23 <agantal.jalal@gmail.com>
2015-02-05 06:29:30 +0800

    Created partial UI for server app

--- RCCarServer/app/src/main/java/ph/edu/msuiit/rccarserver/ServerActivity.java
+++ RCCarServer/app/src/main/java/ph/edu/msuiit/rccarserver/ServerActivity.java
@@ -5,15 +5,16 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import ph.edu.msuiit.rccarserver.utils.KitKatTweaks;
 
 public class ServerActivity extends ActionBarActivity {
-
+    DiscoveryServer dThread;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_server);
+        KitKatTweaks.enableStatusBarTint(this);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
