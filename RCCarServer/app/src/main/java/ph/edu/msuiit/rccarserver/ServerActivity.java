69b3ba6e7b9dce7e3f9db9223f1293a3a46a4894
jal23 <agantal.jalal@gmail.com>
2015-02-05 19:22:24 +0800

    added icon refresh menu item

--- RCCarServer/app/src/main/java/ph/edu/msuiit/rccarserver/ServerActivity.java
+++ RCCarServer/app/src/main/java/ph/edu/msuiit/rccarserver/ServerActivity.java
@@ -43,12 +43,11 @@ public class ServerActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }
@@ -56,7 +55,7 @@ public class ServerActivity extends ActionBarActivity {
     public void startListening(View view) throws UnknownHostException {
         DiscoveryServer dThread; dThread = new DiscoveryServer("DiscoveryServer");
         dThread.start();
-        KitKatTweaks.enableStatusBarTint(this);
+        setContentView(R.layout.activity_start_listening);
         displayConnectionDetails();
     }
 
